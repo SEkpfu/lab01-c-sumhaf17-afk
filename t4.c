5d68e20cb0094e5541d1271f8cd3fd0ec6c37e57
sumhaf17-afk <sumhaf17@gmail.com>
2025-10-05 23:28:29 +0300

    Update t4.c

--- t4.c
+++ t4.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-//#include <locale.h>
-int main()
-{
+#include <math.h>
+
+int main() {
   double x, y;
   printf("x= ");
   scanf("%lf", &x);
-  double x2 = x * x;
-  double x4 = x2 * x2;
-  double x8 = x4 * x4;
-  double x10 = x8 * x2;
-  double x11 = x10 * x;
-  y = x11 - x + x2 - 2;
-  printf("%.2lf\n", y);
+  y = pow(14, 6) - pow(x, 5) + pow(x, 4) - x + 2;
+  printf("y = %.31f\n", y);
   return 0;
 }
